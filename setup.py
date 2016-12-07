fce238eb75639856174650142c974700f4128782
mcueto <cueto@live.cl>
2016-12-06 23:31:38 -0300

    minor changes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'rest_framework_borderkeeper',
   packages = ['rest_framework_borderkeeper'],
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'Django Rest Framework Library to manage user filters and permissions with different entities',
   author = 'Marcelo Cueto',
   author_email = 'yo@marcelocueto.cl',
   url = 'https://github.com/mcueto/djangorestframework-borderkeeper',
-  download_url = 'https://github.com/mcueto/djangorestframework-borderkeeper/tarball/0.0.1',
+  download_url = 'https://github.com/mcueto/djangorestframework-borderkeeper/tarball/0.0.2',
   keywords = ['rest framework', 'django', 'security'],
   classifiers=[
       'Environment :: Web Environment',
