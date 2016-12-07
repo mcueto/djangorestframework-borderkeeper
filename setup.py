4ef76dd5f0955d9c1f36802bc2d1a444c4cb3da0
mcueto <cueto@live.cl>
2016-12-06 23:20:06 -0300

    Added setup for PyPi

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'rest_framework_borderkeeper',
+  packages = ['rest_framework_borderkeeper'],
+  version = '0.0.1',
+  description = 'Django Rest Framework Library to manage user filters and permissions with different entities',
+  author = 'Marcelo Cueto',
+  author_email = 'yo@marcelocueto.cl',
+  url = 'https://github.com/mcueto/djangorestframework-borderkeeper',
+  download_url = 'https://github.com/mcueto/djangorestframework-borderkeeper/tarball/0.0.1',
+  keywords = ['rest framework', 'django', 'security'],
+  classifiers=[
+      'Environment :: Web Environment',
+      'Framework :: Django',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Topic :: Internet :: WWW/HTTP',
+  ],
+  install_requires = [
+      'djangorestframework>=1.9.0',
+      'django-filter',
+  ],
+)
